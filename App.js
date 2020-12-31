668df6d910f473a081eb5f55f479fd41590c944b
Sven Le Cann <sven.lecann@live.fr>
2020-12-31 01:51:07 +0100

    Section 8 - 170

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 import ShopNavigator from './navigation/ShopNavigator';
 import productsReducer from './store/reducers/products';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -11,9 +13,17 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <ShopNavigator />
-    </Provider>
-  );
+  let [fontsLoaded] = useFonts({
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Provider store={store}>
+        <ShopNavigator />
+      </Provider>
+    );
+  }
 }
