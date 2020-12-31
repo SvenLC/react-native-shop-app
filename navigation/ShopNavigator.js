4f76f5849ec0819bf7791b0b8b8788647a07ac5d
Sven Le Cann <sven.lecann@live.fr>
2020-12-31 01:06:34 +0100

    Section 8 - 168

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,13 +1,14 @@
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Platform } from 'react-native';
-
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import Colors from '../constants/Colors';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
   },
   {
     defaultNavigationOptions: {
