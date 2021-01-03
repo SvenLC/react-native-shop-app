596bb174d8ab4e2a740daf80c85429f189cc3ed6
Sven Le Cann <sven.lecann@live.fr>
2021-01-03 17:48:15 +0100

    Section 8 - 178

--- models/order.js
+++ models/order.js
@@ -1,9 +1,22 @@
+import Moment from 'moment';
+
 class Order {
   constructor(id, items, totalAmout, date) {
     this.id = id;
     this.items = items;
     this.totalAmount = totalAmout;
     this.date = date;
+  }
+
+  get readableDate() {
+    // return this.date.toLocaleDateString('fr-FR', {
+    //   year: 'numeric',
+    //   month: 'long',
+    //   day: 'numeric',
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    // });
+    return Moment(this.date).format('MMMM Do YYYY, hh:mm');
   }
 }
 
